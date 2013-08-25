4c48416a29abc4a13d310c7902236dbef4d512e7
AA <abergman@Minty.(none)>
2013-08-24 23:39:13 -0400

    yes confirmation in apt-get distribute issue

--- minty/mypythonsetup.sh
+++ minty/mypythonsetup.sh
@@ -2,15 +2,15 @@ set -e -x
 
 #mkdir /apps
 #chown minty:minty /apps
-apt-get install libpng-dev libfreetype6 libfreetype6-dev \
+apt-get install -y libpng-dev libfreetype6 libfreetype6-dev \
   libblas-dev liblapack-dev gfortran \
   mercurial libncurses-dev cscope \
   libgtk2.0-dev libxt-dev \
   lua5.2-dev ruby-dev tcl-dev lua5.2 ruby tcl \
-  python-pip
+  python-dev python-pip
+easy_install -U distribute
 pip install numpy
 pip install matplotlib
-pip install distribute
 pip install scipy
 pip install scikit-learn
 pip install pylint pandas ipython
