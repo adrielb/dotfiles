f2e468eab5732948c1b26c8c817fecdb75a08093
Adriel Bergmann <adrielb@users.noreply.github.com>
2013-08-24 22:27:13 -0400

    initial commit

---
+++ minty/mypythonsetup.sh
@@ -0,0 +1,12 @@
+mkdir /apps
+chown minty:minty /apps
+apt-get install libpng-dev libfreetype6 libfreetype6-dev libblas-dev liblapack-dev gfortran
+apt-get install mercurial libncurses-dev cscope
+apt-get install libgtk2.0-dev libxt-dev lua5.2-dev ruby-dev tcl-dev lua5.2 ruby tcl
+apt-get install python-pip
+pip install numpy
+pip install matplotlib
+pip install distribute
+pip install scipy
+pip install scikit-learn 
+pip install pylint pandas ipython
