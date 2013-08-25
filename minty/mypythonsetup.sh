e2549803e3856df8d5d6b6bcedae4b831d8c22eb
Adriel Bergmann <adrielb@users.noreply.github.com>
2013-08-24 22:55:09 -0400

    joined all apt install lines

--- minty/mypythonsetup.sh
+++ minty/mypythonsetup.sh
@@ -1,12 +1,16 @@
-mkdir /apps
-chown minty:minty /apps
-apt-get install libpng-dev libfreetype6 libfreetype6-dev libblas-dev liblapack-dev gfortran
-apt-get install mercurial libncurses-dev cscope
-apt-get install libgtk2.0-dev libxt-dev lua5.2-dev ruby-dev tcl-dev lua5.2 ruby tcl
-apt-get install python-pip
+set -e -x
+
+#mkdir /apps
+#chown minty:minty /apps
+apt-get install libpng-dev libfreetype6 libfreetype6-dev \
+  libblas-dev liblapack-dev gfortran \
+  mercurial libncurses-dev cscope \
+  libgtk2.0-dev libxt-dev \
+  lua5.2-dev ruby-dev tcl-dev lua5.2 ruby tcl \
+  python-pip
 pip install numpy
 pip install matplotlib
 pip install distribute
 pip install scipy
-pip install scikit-learn 
+pip install scikit-learn
 pip install pylint pandas ipython
