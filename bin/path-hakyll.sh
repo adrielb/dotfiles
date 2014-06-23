43463c5bc1eabfec5a512adbafc71e53807d5de0
Adriel Bergmann <adrielb@users.noreply.github.com>
2014-06-23 15:40:39 -0400

    exporting path environment for yesod and hakyll

---
+++ bin/path-hakyll.sh
@@ -0,0 +1,9 @@
+HAKYLL_SANDBOX=~/apps/hakyll
+export PATH=$HAKYLL_SANDBOX/.cabal-sandbox/bin:$PATH
+export GHC_PACKAGE_PATH=$HAKYLL_SANDBOX/.cabal-sandbox/x86_64-linux-ghc-7.4.1-packages.conf.d:
+
+#export alias ghc="ghc -package-conf $GHC_PACKAGE_PATH"
+
+cd ~/projects/hakyll
+
+bash
