e3fec5f473eb8f37bfe0bce328f435e18f2b031c
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-05-06 14:45:08 -0400

    deleted unused executables

--- bin/path-yesod.sh
+++
@@ -1,7 +0,0 @@
-YESOD_SANDBOX=~/apps/yesod-bin
-export PATH=$YESOD_SANDBOX/.cabal-sandbox/bin:$PATH
-export GHC_PACKAGE_PATH=$YESOD_SANDBOX/.cabal-sandbox/x86_64-linux-ghc-7.4.1-packages.conf.d:
-
-cd ~/projects/yesod
-
-bash
