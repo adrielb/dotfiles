17d47d0dcacdf3f0558070c05755537ae4fe8026
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-05-29 12:55:42 -0400

    debug: log files needed abosulte path
    if script changes directory, relative paths to log files were lost
    when startLog() called again.

--- deb/logger.sh
+++ deb/logger.sh
@@ -6,12 +6,12 @@ set -x # log all cmds before executing
 APPS=~/apps/
 
 
-LOGDIR=logs
+LOGDIR=$PWD/logs
 LOGFILE=$0-`date '+%F-%T'`
 LOG=$LOGDIR/$LOGFILE
 mkdir -p $LOGDIR
-ln -sf $LOGFILE.stdout $LOGDIR/$0.stdout
-ln -sf $LOGFILE.stderr $LOGDIR/$0.stderr
+ln -sf $LOGFILE.stdout logs/$0.stdout
+ln -sf $LOGFILE.stderr logs/$0.stderr
 
 startLog() {
   exec > >(tee -a $LOG.stdout) \
