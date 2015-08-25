8ca30ac841bab9ac1513b91eaff847bbadd85e64
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-08-25 14:13:39 -0400

    cmdstan install script

---
+++ deb/stan.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+source logger.sh
+
+cd ${APPS}
+if [ ! -d "git" ]; then
+  git clone https://github.com/stan-dev/cmdstan.git
+fi
+cd cmdstan/
+git fetch
+git co v2.7.0  
+make clean
+make stan-update
+make -j$NUM_PROCS build
