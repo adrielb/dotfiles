12a819db120799593746161e352f2d08a3ebca71
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-09-29 11:45:40 -0400

    open data pdf in other monitor

--- scripts/bay.sh
+++ scripts/bay.sh
@@ -8,6 +8,8 @@ if [[ $1 == "repl" ]]; then
 else
   export DISPLAY=":0.1"
   gnome-terminal -x ~/bay.sh repl &
+  export DISPLAY=":0.2"
+  zathura ~/Downloads/barrestin.pdf &
   sleep 1
   nvim -c "normal gg g;zx" regression.R
   exec bash
