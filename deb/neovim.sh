c88f730f214d5315d8d09db003d1a7e7d711e921
Adriel Bergmann <adrielb@users.noreply.github.com>
2016-09-27 14:44:41 -0400

    update pip3 neovim too

--- deb/neovim.sh
+++ deb/neovim.sh
@@ -16,6 +16,6 @@ git co v0.1.5
 rm -rf build/ .deps
 make clean
 make -j$NUM_PROCS CMAKE_BUILD_TYPE=Release
-su -c 'make install && pip install --upgrade neovim'
+su -c 'make install && pip install --upgrade neovim && pip3 install --upgrade neovim'
 stopLog
 nvim +PlugUpdate
