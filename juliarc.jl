16bf3ef34d3de785f96ab57305e87186043b52e7
Adriel Bergmann <adrielb@users.noreply.github.com>
2016-11-11 15:20:35 -0500

    juliarc: run init file in pwd

--- juliarc.jl
+++ juliarc.jl
@@ -1,11 +1,15 @@
 # isinteractive() test not working?
-ENV["DISPLAY"]=":0.1"
+# ENV["DISPLAY"]=":0.1"
+if isfile("init.jl")
+  include(joinpath(pwd(),"init.jl"))
+end
 
 if isinteractive()
   println("Interactive!")
-  ENV["DISPLAY"]=":0.1"
+  # ENV["DISPLAY"]=":0.1"
   if isfile("init.jl")
     require("init.jl")
   end
 end
+
 
