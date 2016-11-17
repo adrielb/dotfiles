490781f486652af25f6feb8ebc16577abce5ac24
Adriel Bergmann <adrielb@users.noreply.github.com>
2016-11-16 21:39:36 -0500

    python package tensorflow and edward

--- deb/python.sh
+++ deb/python.sh
@@ -3,6 +3,8 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp35-cp35m-linux_x86_64.whl
+
 # easy_install -U distribute
 pip install --user --upgrade \
   pip \
@@ -28,7 +30,8 @@ pip install --user --upgrade \
   neovim \
   keras \
   spacy \
-
+  edward \
+  $TF_BINARY_URL
 
   # prettyplotlib \
   # mpld3 \
@@ -38,10 +41,4 @@ pip install --user --upgrade \
   # jinja2 \
 
 # python -m spacy.en.download all
-
-
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp35-cp35m-linux_x86_64.whl
-
-pip3 install --user --upgrade $TF_BINARY_URL
-
 
