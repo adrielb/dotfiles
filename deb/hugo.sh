931cbf8b8763a79f935d6bb934e201c55cf653fc
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-01-20 10:07:54 -0500

    hugo v0.33

--- deb/hugo.sh
+++ deb/hugo.sh
@@ -1,8 +1,12 @@
 cd /tmp
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.32.4/hugo_0.32.4_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.33/hugo_0.33_Linux-64bit.tar.gz
 
 tar zxvf *.tar.gz
 
+hugo version
+
 mv hugo ~/.local/bin
+
+hugo version
 
