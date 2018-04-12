361ed49a2739f77f51dc1fcc1d6ac45a64d7a83e
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-04-12 13:13:02 -0400

    hugo v0.38.2

--- deb/hugo.sh
+++ deb/hugo.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.33/hugo_0.33_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.38.2/hugo_0.38.2_Linux-64bit.tar.gz
 
 tar zxvf *.tar.gz
 
