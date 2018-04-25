d590c9d34e3419a049a89b34856cae4c55640873
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-04-25 14:53:28 -0400

    python: dash, bokeh, joblib, pytorch 0.4

--- deb/python.sh
+++ deb/python.sh
@@ -45,7 +45,14 @@ pip3 install --user --upgrade \
   pyro-ppl \
   python-language-server \
   xlrd \
-  # http://download.pytorch.org/whl/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl \
+  joblib \
+  dash \
+  dash-renderer \
+  dash-core-components \
+  dash-html-components \
+  bokeh \
+  holoviews \
+  # http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl 
   # torchvision
 
   # prettyplotlib \
