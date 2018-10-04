9a51ad45714df7841b2aa7dbc9b9b8b24efff10e
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-10-04 14:37:16 -0400

    python: julia and jupytext

--- deb/python.sh
+++ deb/python.sh
@@ -53,6 +53,8 @@ pip3 install --user --upgrade \
   dask[complete] \
   attrs \
   cvxpy \
+  julia \
+  jupytext \
   # http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl 
   # torchvision
 
