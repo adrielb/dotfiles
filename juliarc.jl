61a18f20184b531296b36d140c49c29aa47c8c87
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-11-13 10:22:03 -0500

    juliarc uses atreplinit() check

--- juliarc.jl
+++ juliarc.jl
@@ -1,19 +1,17 @@
-# isinteractive() test not working?
 # ENV["DISPLAY"]=":0.1"
-if isfile("init.jl")
-  include(joinpath(pwd(),"init.jl"))
+
+atreplinit() do repl
+    try
+        @eval push!(LOAD_PATH, pwd())
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+        @eval using OhMyREPL
+    catch
+    end
+    if isfile("init.jl")
+      include(joinpath(pwd(),"init.jl"))
+    end
 end
 
-if isinteractive()
-  println("Interactive!")
-  # ENV["DISPLAY"]=":0.1"
-  if isfile("init.jl")
-    require("init.jl")
-  end
-end
 
-if isdir(Pkg.dir("OhMyREPL"))
-    @eval using OhMyREPL
-else
-    warn("OhMyREPL not installed")
-end
+ENV["PYTHON"]="/usr/local/bin/python3"
