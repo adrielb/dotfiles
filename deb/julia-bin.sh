bb56769983b0467eba19ea547607326a4259c7b0
Adriel Bergmann <adrielb@users.noreply.github.com>
2018-11-13 10:04:29 -0500

    julia 1.0.2

--- deb/julia-bin.sh
+++ deb/julia-bin.sh
@@ -1,8 +1,8 @@
 cd ~/apps/julia-bin/
 
-JULIA=julia-1.0.1
+JULIA=julia-1.0.2
 
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.1-linux-x86_64.tar.gz
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/$JULIA-linux-x86_64.tar.gz
 
 tar zxvf $JULIA-linux-x86_64.tar.gz
 
