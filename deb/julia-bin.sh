a213830d0459e7fd3c92fcfc23394be8eede7ffd
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-01-18 13:49:24 -0500

    julia-1.0.3

--- deb/julia-bin.sh
+++ deb/julia-bin.sh
@@ -1,16 +1,13 @@
+JULIA=julia-1.0.3
 cd ~/apps/julia-bin/
-
-JULIA=julia-1.0.2
-
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/$JULIA-linux-x86_64.tar.gz
-
 tar zxvf $JULIA-linux-x86_64.tar.gz
+ln -sf $JULIA current
+julia
 
 cd ~/apps/local/bin
+ln -sf ~/apps/julia-bin/current/bin/julia
 
-ln -sf ~/apps/julia-bin/$JULIA/bin/julia
-
-julia
 
 ctags-exuberant \
   --recurse \
