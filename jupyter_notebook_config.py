9a9b892fc899f2d5083dfd05c1a53bfe87f598a4
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-10-12 20:47:21 -0400

    julia webio config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -698,3 +698,19 @@ c.ContentsManager.default_cell_metadata_filter = "-all"
 #  
 #  By default, all installed kernels are allowed.
 #c.KernelSpecManager.whitelist = set()
+
+###JULIA-WEBIO-CONFIG-BEGIN
+# Add the path to WebIO/deps so that we can load the `jlstaticserve` extension.
+import os, sys, warnings
+webio_deps_dir = "/home/abergman/.julia/packages/WebIO/2mZPb/deps"
+if os.path.isfile(os.path.join(webio_deps_dir, "jlstaticserve.py")):
+    sys.path.append(webio_deps_dir)
+else:
+    warning_msg = (
+        'Directory %s could not be found; WebIO.jl will not work as expected. '
+        + 'Make sure WebIO.jl is installed correctly (try running '
+        + 'Pkg.add("WebIO") and Pkg.build("WebIO") from the Julia console to '
+        + 'make sure it is).'
+    ) % webio_deps_dir
+    warnings.warn(warning_msg)
+###JULIA-WEBIO-CONFIG-END
