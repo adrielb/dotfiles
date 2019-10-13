d55c4420ae35cb705a57e885f6b7e103acd82697
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-10-12 20:50:39 -0400

    sleep in script so vim doesnt block

--- bin/parse_julia_repl.sh
+++ bin/parse_julia_repl.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-tmux capture-pane -p -S -20 -J | awk -v RS='julia> ' 'BEGIN { ORS=RS }  {b=a; a=$$0}  END { print "BASE: /home/abergman/apps/julia-bin/current/share/julia/base\njulia> "b }'
+sleep 1
+tmux capture-pane -p -S -20 -J | \
+  awk --assign RS='julia> ' \
+    'BEGIN { ORS=RS }  
+           {b=a; a=$$0}  
+       END { print "BASE: /home/abergman/apps/julia-bin/current/share/julia/base\njulia> "b }'
