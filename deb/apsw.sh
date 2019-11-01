ae6f0821400b166071fa489e041cd9c51798894a
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-10-31 21:21:42 -0400

    APSW 3.29.0-r1

--- deb/apsw.sh
+++ deb/apsw.sh
@@ -1,4 +1,4 @@
-RELEASE='3.22.0-r1'
+RELEASE='3.29.0-r1'
 
 cd /tmp
 
@@ -8,7 +8,18 @@ unzip apsw-$RELEASE.zip
 
 cd apsw-$RELEASE
 
-python3 setup.py fetch --all \
+python3 setup.py fetch --missing-checksum-ok --all \
   build --enable-all-extensions \
   install --user \
   test
+
+
+python3 -c '
+import apsw
+print( "apsw:   ", apsw.apswversion())
+print( "sqlite: ", apsw.sqlitelibversion())
+conn = apsw.Connection(":memory:")
+cursor = conn.cursor()
+cursor.execute("CREATE VIRTUAL TABLE testing USING fts5(data);")
+cursor.execute("SELECT json(?)", (1337,)).fetchone()
+'
