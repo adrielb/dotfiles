ba367039b54e842152fa9bb936cbf28e9e723dd7
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-11-13 22:33:49 -0500

    unision install

---
+++ deb/unison.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ./logger.sh
+
+APPDIR=~/apps/unison
+if [ ! -d "$APPDIR" ]; then
+	git clone https://github.com/bcpierce00/unison.git ${APPDIR}
+fi
+
+cd ${APPDIR}
+
+git pull
+
+make -j${NUM_PROCS}
+
+
