c96b22ba43a06812d2a7b23497bbe011f79657c3
Adriel Bergmann <adrielb@users.noreply.github.com>
2019-12-07 21:40:29 -0500

    capture repl from specific tmux session

--- bin/parse_julia_repl.sh
+++ bin/parse_julia_repl.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+SESSION=$1
+WINDOW=$2
+
 sleep 1
-tmux capture-pane -p -S -20 -J | \
+tmux capture-pane -p -S -20 -J -t $SESSION:$WINDOW | \
   awk --assign RS='julia> ' \
     'BEGIN { ORS=RS }  
            {b=a; a=$$0}  
