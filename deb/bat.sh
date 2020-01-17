1c18e6be361eac9e05d824fef673c58401835e9f
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-01-17 01:59:47 -0500

    bat install

---
+++ deb/bat.sh
@@ -0,0 +1,10 @@
+cd /tmp
+
+mkdir bat
+
+cd bat
+
+wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat-v0.12.1-x86_64-unknown-linux-musl.tar.gz
+
+tar xvf /tmp/bat/bat-v0.12.1-x86_64-unknown-linux-musl.tar.gz
+
