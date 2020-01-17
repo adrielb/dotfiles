d64449d424826c216695319868d9c907921c898c
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-01-17 02:04:47 -0500

    hugo 0.62.2

--- deb/hugo.sh
+++ deb/hugo.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+set -o pipefail # exit status 0 only if all cmds 0
+set -e # exit immediately upon failure
+set -x # log all cmds before executing
+
+
 # https://github.com/gohugoio/hugo/releases
+
+VERSION=0.62.2
 
 cd /tmp
 
-URL=https://github.com/gohugoio/hugo/releases/download/v0.58.1/hugo_0.58.1_Linux-64bit.tar.gz
+URL=https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
 
 wget $URL
 
