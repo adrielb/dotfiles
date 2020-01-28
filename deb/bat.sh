c5fc9a82d1f74d479a30f66937133cff34ca9235
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-01-27 21:41:20 -0500

    bat v0.12.1 VERSION var

--- deb/bat.sh
+++ deb/bat.sh
@@ -3,10 +3,11 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+VERSION=v0.12.1
 mkdir -p /tmp/bat
 cd /tmp/bat
 
-wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat-v0.12.1-x86_64-unknown-linux-musl.tar.gz
+wget https://github.com/sharkdp/bat/releases/download/$VERSION/bat-$VERSION-x86_64-unknown-linux-musl.tar.gz
 
 tar xvf bat*.tar.gz
 
@@ -15,4 +16,6 @@ rm bat*.tar.gz
 cd bat*
 
 mv bat ~/apps/local/bin
+
+bat --version
 
