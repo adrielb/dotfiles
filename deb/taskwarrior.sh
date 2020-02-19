96b06512ff3d316943693db517a32d5ded94d39b
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-02-19 13:22:29 -0500

    taskwarrior 2.5.1

---
+++ deb/taskwarrior.sh
@@ -0,0 +1,19 @@
+# https://github.com/GothenburgBitFactory/taskwarrior
+. ./logger.sh
+
+TWDIR=${APPS}/taskwarrior
+if [ ! -d "${TWDIR}" ]; then
+  git clone https://github.com/GothenburgBitFactory/taskwarrior.git ${TWDIR}
+fi
+
+# apt-get install libgnutls28-dev uuid-dev
+
+cd ${TWDIR}
+git fetch
+git co master
+cmake \
+  -DCMAKE_BUILD_TYPE=release . \
+  -DCMAKE_INSTALL_PREFIX:PREFIX=${APPS}/local
+make -j$NUM_PROCS
+make install
+
