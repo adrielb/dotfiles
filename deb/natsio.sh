078f21cc20510db2715d91b9338d451d8b7f8fab
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-03-12 21:33:54 -0400

    nats-server v2.1.4

---
+++ deb/natsio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o pipefail # exit status 0 only if all cmds 0
+set -e # exit immediately upon failure
+set -x # log all cmds before executing
+
+VERSION=v2.1.4
+
+mkdir -p /tmp/nats
+cd /tmp/nats
+
+wget https://github.com/nats-io/nats-server/releases/download/$VERSION/nats-server-$VERSION-linux-amd64.zip
+
+unzip nats-server-$VERSION-linux-amd64.zip
+
+cd nats-server-$VERSION-linux-amd64
+
+mv nats-server ~/apps/local/bin
+
+nats-server --version
