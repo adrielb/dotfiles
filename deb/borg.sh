68ec6895d6ec14cd28191d0e850e4b81d60caf63
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-03-22 19:23:53 -0400

    borg 1.1.11

---
+++ deb/borg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o pipefail # exit status 0 only if all cmds 0
+set -e # exit immediately upon failure
+set -x # log all cmds before executing
+
+
+cd ~/apps/local/bin
+
+wget https://github.com/borgbackup/borg/releases/download/1.1.11/borg-linux64
+
+wget https://github.com/borgbackup/borg/releases/download/1.1.11/borg-linux64.asc
+
+gpg --recv-keys 9F88FB52FAF7B393
+gpg --verify borg-linux64.asc borg-linux64
+
+mv borg-linux64 borg
+
+rm borg-linux64.asc
