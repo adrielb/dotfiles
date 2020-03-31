ed2efd9ee5159eb8743652622ffc195bcf5c167f
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-03-31 19:37:42 -0400

    show borgmatic log in tmux tab

--- tmux.sh
+++ tmux.sh
@@ -9,6 +9,8 @@ if tmux has-session -t $NAME; then
 else
   tmux new-session -A -s $NAME \; \
     new-window -n 'bashr' \; \
-      send-keys 'su -' C-m \;
+      send-keys 'su -' C-m \; \
+    new-window -n 'borgmatic.log' \; \
+      send-keys 'tail -F ~/log/borgmatic.log' C-m \;
 fi
 
