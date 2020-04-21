cbc6c0371d422549bb8bdc50b03019ff2c064235
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-04-20 21:05:06 -0400

    redis 5.0.8

---
+++ deb/redis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o pipefail # exit status 0 only if all cmds 0
+set -e # exit immediately upon failure
+set -x # log all cmds before executing
+
+VERSION=5.0.8
+
+cd /tmp
+
+wget http://download.redis.io/releases/redis-$VERSION.tar.gz
+
+tar xzf redis-$VERSION.tar.gz
+
+cd redis-$VERSION
+
+make
+
+cp src/redis-* ~/apps/local/bin
