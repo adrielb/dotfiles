d41658cfc5ced9bd668eaae83354921978618337
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-04-22 15:25:05 -0400

    python redis

--- deb/python.sh
+++ deb/python.sh
@@ -53,6 +53,7 @@ pip3 install --user --upgrade \
   requests_oauthlib \
   asyncio-nats-client \
   borgmatic \
+  redis \
   huey \
   arq \
   # http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl 
