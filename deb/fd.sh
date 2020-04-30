8b794bae089377f283df0345e251dde62b75b79c
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-04-30 17:11:02 -0400

    bash scripts cant use command line !$ shortcut

--- deb/fd.sh
+++ deb/fd.sh
@@ -3,11 +3,12 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+VERSION=8.0.0
 
 mkdir -pv /tmp/fd
 
-cd !$
+cd /tmp/fd
 
-wget --no-clobber https://github.com/sharkdp/fd/releases/download/v8.0.0/fd-musl_8.0.0_amd64.deb
+wget --no-clobber https://github.com/sharkdp/fd/releases/download/v${VERSION}/fd-musl_${VERSION}_amd64.deb
 
 sudo apt install ./*.deb
