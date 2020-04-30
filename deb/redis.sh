b19a5b52934d5dc02cc5c720cb3d5984faf7438a
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-04-30 17:04:03 -0400

    redis 6.0.0

--- deb/redis.sh
+++ deb/redis.sh
@@ -3,18 +3,20 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
-VERSION=5.0.8
+VERSION=6.0.0
 
 cd /tmp
 
-wget http://download.redis.io/releases/redis-$VERSION.tar.gz
+wget --no-clobber http://download.redis.io/releases/redis-$VERSION.tar.gz
 
 tar xzf redis-$VERSION.tar.gz
 
 cd redis-$VERSION
 
-make
+make -j
 
-cp src/redis-* ~/apps/local/bin
+REDIS=$(find src/ -type f -executable -name 'redis-*')
+
+cp $REDIS ~/apps/local/bin
 
 redis-server --version
