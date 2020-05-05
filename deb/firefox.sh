b8400e4d9d11fa2fbc8657c75363d8a9f1bf9a27
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-05-05 11:29:09 -0400

    firefox 76

--- deb/firefox.sh
+++ deb/firefox.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+set -o pipefail # exit status 0 only if all cmds 0
+set -e # exit immediately upon failure
+set -x # log all cmds before executing
+
+
+cd ~/apps
+
+tar xvf ~/Downloads/firefox-76.0.tar.bz2
+
+exit 0
+
+# old install:
 # https://mozilla.debian.net/
 
 # /etc/apt/sources.list
