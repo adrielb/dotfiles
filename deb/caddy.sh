f076a6d6c95a24259d18b1500980a1824165e7a7
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-08-30 16:46:35 -0400

    caddy 2.1.1

--- deb/caddy.sh
+++ deb/caddy.sh
@@ -5,7 +5,10 @@ set -x # log all cmds before executing
 
 mkdir /tmp/caddy
 cd /tmp/caddy
-wget --no-clobber "https://github.com/caddyserver/caddy/releases/download/v2.0.0/caddy_2.0.0_linux_amd64.tar.gz"
+
+wget --no-clobber "https://github.com/caddyserver/caddy/releases/download/v2.1.1/caddy_2.1.1_linux_amd64.tar.gz"
 
 tar zxvf caddy*.tar.gz
 cp caddy ~/apps/local/bin
+
+caddy version
