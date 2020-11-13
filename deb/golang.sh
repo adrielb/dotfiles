597023280b49634b159c55a487a93c5b179e0f3a
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-11-13 11:39:19 -0500

    go 1.15.5

--- deb/golang.sh
+++ deb/golang.sh
@@ -3,13 +3,17 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
-VERSION=1.14.6
+# https://golang.org/dl/
+
+VERSION=1.15.5
 
 cd /tmp
 
 wget --no-clobber https://golang.org/dl/go$VERSION.linux-amd64.tar.gz
 
 sudo tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
+
+go version
 
 exit 0
 
