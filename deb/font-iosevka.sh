9bf6ee18fe7784751f00e6de2fd82a0a6392c05b
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-12-03 18:24:57 -0500

    iosevka v4.0.0

--- deb/font-iosevka.sh
+++ deb/font-iosevka.sh
@@ -3,13 +3,17 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
-mkdir /tmp/iosevka
+# https://github.com/be5invis/Iosevka/releases
+
+mkdir -p /tmp/iosevka
 cd /tmp/iosevka
 
-wget --no-clobber https://github.com/be5invis/Iosevka/releases/download/v3.0.1/ttf-iosevka-fixed-3.0.1.zip
+VERSION="4.0.0"
+                  
+wget --no-clobber https://github.com/be5invis/Iosevka/releases/download/v$VERSION/ttf-iosevka-fixed-$VERSION.zip
 
-unzip ttf-iosevka*.zip
+unzip -f ttf-iosevka*.zip
 
-cp ttf/iosevka-fixed-regular.ttf ~/.fonts/
+cp iosevka-fixed-regular.ttf ~/.fonts/
 
 fc-cache --force --verbose
