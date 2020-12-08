c76b402c50e9c9f63a27bd118eb4f5a5bf02c9a1
Adriel Bergmann <adrielb@users.noreply.github.com>
2020-12-08 17:30:11 -0500

    nomad 1.0.0

--- whisperquiet/nomad.sh
+++ whisperquiet/nomad.sh
@@ -4,12 +4,19 @@ set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
 # https://github.com/hashicorp/nomad/releases
+# https://www.nomadproject.io/downloads
 
 mkdir -p /tmp/nomad
 cd /tmp/nomad
 
-wget --no-clobber https://releases.hashicorp.com/nomad/0.11.3/nomad_0.11.3_linux_amd64.zip
+wget --no-clobber https://releases.hashicorp.com/nomad/1.0.0/nomad_1.0.0_linux_amd64.zip
+
+wget --no-clobber https://releases.hashicorp.com/nomad/1.0.0/nomad_1.0.0_SHA256SUMS
+
+sha256sum --check --ignore-missing *_SHA256SUMS
 
 unzip nomad*zip
 
 cp nomad ~/apps/local/bin
+
+nomad --version
