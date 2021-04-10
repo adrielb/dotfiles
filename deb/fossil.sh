1797cc37d4f00669a4d543e28420a0ebb5921e95
Adriel Bergmann <adrielb@users.noreply.github.com>
2021-04-10 19:21:31 -0400

    fossil 2.15.1

--- deb/fossil.sh
+++ deb/fossil.sh
@@ -3,12 +3,19 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+# https://fossil-scm.org/home/uv/download.html
+
+VER=2.15.1
+FOSSIL_GZ=fossil-linux-x64-$VER.tar.gz
+URL=https://fossil-scm.org/home/uv/$FOSSIL_GZ
 
 mkdir /tmp/fossil
 cd /tmp/fossil
 
-wget --no-clobber https://www.fossil-scm.org/fossil/uv/fossil-linux-x64-2.11.1.tar.gz
+wget --no-clobber $URL
 
-tar zxvf fossil*.tar.gz
+tar zxvf $FOSSIL_GZ
 
 cp fossil ~/apps/local/bin
+
+fossil version
