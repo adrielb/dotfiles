824914e419ee65f62eb450a37031f07dc6c2ec69
AB <ab@users.noreply.github.com>
2021-04-22 19:10:05 -0400

    fd 8.2.1

--- deb/fd.sh
+++ deb/fd.sh
@@ -3,12 +3,14 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
-VERSION=8.0.0
+# https://github.com/sharkdp/fd/releases
 
-mkdir -pv /tmp/fd
+VERSION=8.2.1
 
-cd /tmp/fd
+mkdir -pv /tmp/fd && cd "$_"
 
 wget --no-clobber https://github.com/sharkdp/fd/releases/download/v${VERSION}/fd-musl_${VERSION}_amd64.deb
 
 sudo apt install ./*.deb
+
+fd --version
