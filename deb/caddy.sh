de3663f5f9070676ed00b942ced99f1d00fdd85d
AB <ab@users.noreply.github.com>
2021-05-06 23:44:17 -0400

    caddy v2.3.0

--- deb/caddy.sh
+++ deb/caddy.sh
@@ -3,10 +3,14 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+# https://github.com/caddyserver/caddy/releases
+
+VERSION=2.3.0
+
 mkdir /tmp/caddy
 cd /tmp/caddy
 
-wget --no-clobber "https://github.com/caddyserver/caddy/releases/download/v2.1.1/caddy_2.1.1_linux_amd64.tar.gz"
+wget --no-clobber "https://github.com/caddyserver/caddy/releases/download/v$VERSION/caddy_${VERSION}_linux_amd64.tar.gz"
 
 tar zxvf caddy*.tar.gz
 cp caddy ~/apps/local/bin
