316ae5bdcd3992495d0d37d722a8e145807bbbea
AB <ab@users.noreply.github.com>
2021-05-16 17:52:58 -0400

    mdcat 0.22.4

--- deb/mdcat.sh
+++ deb/mdcat.sh
@@ -3,10 +3,12 @@ set -o pipefail # exit status 0 only if all cmds 0
 set -e # exit immediately upon failure
 set -x # log all cmds before executing
 
+VERSION=0.22.4
+
 mkdir -p /tmp/mdcat
 cd /tmp/mdcat
 
-wget https://github.com/lunaryorn/mdcat/releases/download/mdcat-0.15.0/mdcat-0.15.0-x86_64-unknown-linux-musl.tar.gz
+wget --no-clobber https://github.com/lunaryorn/mdcat/releases/download/mdcat-$VERSION/mdcat-$VERSION-x86_64-unknown-linux-musl.tar.gz
 
 tar xvf mdcat*.tar.gz
 
@@ -16,3 +18,4 @@ cd mdcat*
 
 mv mdcat ~/apps/local/bin
 
+mdcat --version
